41ab27c5ecf540564111952f553984710823ea3b
Ashley Wong <ashwong04@gmail.com>
2018-09-16 12:56:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,10 +14,10 @@ class Anagram
     
     list.each do |l|
       list_word = l.split(//).sort
-      if  list_word == sort_word
-       anagram_list << l
-      end
-       return anagram_list
+      list_word == sort_word
+       l
+      #end
+       #return anagram_list
     end
   
   end
